5c8062615d638e35da1086446eb4a8489fbb7f25
dennis-kya80 <94791709+dennis-kya80@users.noreply.github.com>
2021-11-23 21:25:37 +0000

    my first commit

---
+++ PycharmProjects/pythonProject2/tuesday.py
@@ -0,0 +1,22 @@
+print("create accont")
+username=input("enter username")
+password=input("enter password")
+print("your account has been created successfully")
+print("log in")
+username2=input("enter username2")
+password2=input("enter password2")
+if username==username2 and password==password2:
+    print("logged in successfully")
+else:C:\Users\user\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/user/PycharmProjects/pythonProject/tuesday.py
+create account
+enter usernamedennis
+enter passwordabdul
+your account has been created successfully
+log in
+enter username2abdul
+enter password2dennis
+you have entered wrong credentials
+
+Process finished with exit code 0
+
+    print("you have entered wrong credentials")
